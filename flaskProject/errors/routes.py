2fc20bdd2af4a614d55d278eb894eb89419fc4ec
TDTanev21 <TDTanev21@codingburgas.bg>
2025-06-03 09:40:24 +0300

    Add custom error pages
    Co-authored-by: KNKostadinov21 <knkostadinov21@codingburgas.bg>

---
+++ flaskProject/errors/routes.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from pyodbc import IntegrityError
+
+from . import errors_bp
+
+@errors_bp.errorhandler(404)
+def not_found_error(error):
+    return render_template("errors/404.html"), 404
+
+
+@errors_bp.errorhandler(500)
+def internal_error(error):
+    return render_template("errors/500.html"), 500
+
+
+@errors_bp.errorhandler(403)
+def forbidden_error(error):
+    return render_template("errors/403.html"), 403
+
+
+@errors_bp.errorhandler(401)
+def unauthorized_error(error):
+    return render_template("errors/401.html"), 401
+
+@errors_bp.errorhandler(IntegrityError)
+def integrity_error(error):
+    return render_template("errors/already_registered.html"), integrity_error
