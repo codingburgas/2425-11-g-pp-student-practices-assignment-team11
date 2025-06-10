074322930c74da8004c29fe884a7da0b1c6b980f
TDTanev21 <TDTanev21@codingburgas.bg>
2025-06-10 21:32:49 +0300

    Make new API key

--- flaskProject/config.py
+++ flaskProject/config.py
@@ -7,5 +7,5 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SENDGRID_API_KEY = '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'
+    SENDGRID_API_KEY = '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'
     SENDGRID_SENDER = 'tdtanev21@codingburgas.bg'
