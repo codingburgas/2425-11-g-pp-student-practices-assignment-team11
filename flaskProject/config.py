052c3c03706f4ff204f80349220a5bc108b72c84
TDTanev21 <TDTanev21@codingburgas.bg>
2025-06-10 22:28:56 +0300

    Add .env file to hide the secret data

--- flaskProject/config.py
+++ flaskProject/config.py
@@ -1,15 +1,17 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 class Config:
-    SECRET_KEY = "super-secret-key"
-
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
     SQLALCHEMY_DATABASE_URI = (
         'mssql+pyodbc://CloudSA0b7473ff:Teodor21@studypilot.database.windows.net/app'
         '?driver=ODBC+Driver+17+for+SQL+Server'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+    SENDGRID_SENDER = os.environ.get("SENDGRID_SENDER")
     print("SENDGRID_API_KEY:", os.environ.get("SENDGRID_API_KEY"))
-    SENDGRID_SENDER = 'tdtanev21@codingburgas.bg'
+    print("SENDGRID_API_KEY:", os.environ.get("SENDGRID_SENDER"))
+
