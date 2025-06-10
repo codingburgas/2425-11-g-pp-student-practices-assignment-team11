0e7df5960e295967e3395d5c57e43b5eea6a9ca7
TDTanev21 <TDTanev21@codingburgas.bg>
2025-06-10 12:26:15 +0300

    fix env variables

--- flaskProject/config.py
+++ flaskProject/config.py
@@ -3,17 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = (
-        'mssql+pyodbc://CloudSA0b7473ff:Teodor21@studypilot.database.windows.net/app'
-        '?driver=ODBC+Driver+17+for+SQL+Server'
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SENDGRID_API_KEY = os.environ.get(
-        'SENDGRID_API_KEY',
-        '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'
-    )
-    SENDGRID_SENDER = os.environ.get(
-        'SENDGRID_SENDER',
-        'tdtanev21@codingburgas.bg'
-    )
+    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+    SENDGRID_SENDER = os.environ.get('SENDGRID_SENDER')
