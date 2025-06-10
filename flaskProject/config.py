88362d3eff8771cbfc88c4326a03b552760415c7
TDTanev21 <TDTanev21@codingburgas.bg>
2025-06-10 12:37:59 +0300

    change track modification to False

--- flaskProject/config.py
+++ flaskProject/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///database.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
     SENDGRID_SENDER = os.environ.get('SENDGRID_SENDER')
