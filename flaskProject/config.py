717473ea2e0234e7d727934069d47e1380303372
TDTanev21 <TDTanev21@codingburgas.bg>
2025-06-10 13:54:42 +0300

    Fix config.py

--- flaskProject/config.py
+++ flaskProject/config.py
@@ -3,8 +3,17 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///database.db")
+    SQLALCHEMY_DATABASE_URI = (
+        'mssql+pyodbc://CloudSA0b7473ff:Teodor21@studypilot.database.windows.net/app'
+        '?driver=ODBC+Driver+17+for+SQL+Server'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
-    SENDGRID_SENDER = os.environ.get('SENDGRID_SENDER')
+    SENDGRID_API_KEY = os.environ.get(
+        'SENDGRID_API_KEY',
+        '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'
+    )
+    SENDGRID_SENDER = os.environ.get(
+        'SENDGRID_SENDER',
+        'no-reply@studypilot.com'
+    )
